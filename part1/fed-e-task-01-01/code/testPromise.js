513015a78f36cd34bde9dbba7710c14b64b6585a
wb-lx652828 <wb-lx652828@alibaba-inc.com>
2021-08-02 16:59:56 +0800

    feat: 手写Promise —— then方法的链式调用： 分为普通值和对象考虑

--- part1/fed-e-task-01-01/code/testPromise.js
+++ part1/fed-e-task-01-01/code/testPromise.js
@@ -28,33 +28,55 @@
 // })
 
 
-// MyPromise_V3 测试
-const MyPromise = require( './MyPromise_V3' );
-let promise = new MyPromise( (resolve,reject) => {
-	// resolve( '链式调用-- 同步成功' )
+// // MyPromise_V3 测试
+// const MyPromise = require( './MyPromise_V3' );
+// let promise = new MyPromise( (resolve,reject) => {
+// 	// resolve( '多次调用-- 同步成功' )
 
-	// reject('链式调用-- 同步失败')
+// 	// reject('多次调用-- 同步失败')
 
-	setTimeout( () => {
-			resolve('链式调用--异步成功')
-	}, 1000 )
+// 	setTimeout( () => {
+// 			resolve('多次调用--异步成功')
+// 	}, 1000 )
 
+// } )
+
+// promise.then( value => {
+// 	console.log(`value`, value)
+// }, (reason) => {
+// 	console.log(`reason`, reason)
+// } )
+
+// promise.then( value => {
+// 	console.log(`value`, value)
+// }, (reason) => {
+// 	console.log(`reason`, reason)
+// } )
+
+// promise.then( value => {
+// 	console.log(`value`, value)
+// }, (reason) => {
+// 	console.log(`reason`, reason)
+// })
+
+
+// MyPromise_V4 测试
+const MyPromise = require( './MyPromise_V4' );
+let promiseV4 = new MyPromise( (resolve,reject) => {
+	resolve('链式调用-同步成功')
 } )
 
-promise.then( value => {
-	console.log(`value`, value)
-}, (reason) => {
-	console.log(`reason`, reason)
-} )
+// 定义一个用于返回的promise对象进行测试
+other = () => {
+	return new MyPromise( (resolve,reject) => {
+		resolve('other')
+	})
+}
 
-promise.then( value => {
-	console.log(`value`, value)
-}, (reason) => {
-	console.log(`reason`, reason)
-} )
-
-promise.then( value => {
-	console.log(`value`, value)
-}, (reason) => {
-	console.log(`reason`, reason)
+promiseV4.then( (value1) => {
+	console.log( `value1的参数是：`, value1 )
+	// return '这是value1的返回值' // 返回普通纸
+	return other()  // 返回promise对象
+} ).then( (value2) => {
+	console.log(`value2的参数是：`, value2)
 })
