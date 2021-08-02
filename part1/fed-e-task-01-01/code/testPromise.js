28f890846586bcbcc0f4f7923290f851d16a344c
wb-lx652828 <wb-lx652828@alibaba-inc.com>
2021-08-02 17:30:08 +0800

    feat: 手写Promise —— 识别promise自己返回自己，并作出错误提示

--- part1/fed-e-task-01-01/code/testPromise.js
+++ part1/fed-e-task-01-01/code/testPromise.js
@@ -1,4 +1,4 @@
-// MyPromise_V1 测试
+// MyPromise_V1 测试========================================
 // const MyPromise = require( './MyPromise_V1' );
 
 // let promise = new MyPromise( (resolve,reject) => {
@@ -13,7 +13,7 @@
 // } )
 
 
-// // MyPromise_V2 测试
+// // MyPromise_V2 测试========================================
 // const MyPromise = require( './MyPromise_V2' );
 // let promise = new MyPromise( (resolve,reject) => {
 //     setTimeout( () => {
@@ -28,7 +28,7 @@
 // })
 
 
-// // MyPromise_V3 测试
+// // MyPromise_V3 测试========================================
 // const MyPromise = require( './MyPromise_V3' );
 // let promise = new MyPromise( (resolve,reject) => {
 // 	// resolve( '多次调用-- 同步成功' )
@@ -60,23 +60,43 @@
 // })
 
 
-// MyPromise_V4 测试
-const MyPromise = require( './MyPromise_V4' );
-let promiseV4 = new MyPromise( (resolve,reject) => {
-	resolve('链式调用-同步成功')
+// // MyPromise_V4 测试========================================
+// const MyPromise = require( './MyPromise_V4' );
+// let promiseV4 = new MyPromise( (resolve,reject) => {
+// 	resolve('链式调用-同步成功')
+// } )
+
+// // 定义一个用于返回的promise对象进行测试
+// other = () => {
+// 	return new MyPromise( (resolve,reject) => {
+// 		resolve('other')
+// 	})
+// }
+
+// promiseV4.then( (value1) => {
+// 	console.log( `value1的参数是：`, value1 )
+// 	// return '这是value1的返回值' // 返回普通纸
+// 	return other()  // 返回promise对象
+// } ).then( (value2) => {
+// 	console.log(`value2的参数是：`, value2)
+// })
+
+
+
+
+// MYPromise_V5 测试========================================
+const MyPromise = require( './MyPromise_V5' );
+let promise = new MyPromise( (resolve,reject) => {
+	resolve('测试promise返回')
+})
+
+let p1 = promise.then( value => {
+	console.log(`value`, value)
+	return p1
 } )
 
-// 定义一个用于返回的promise对象进行测试
-other = () => {
-	return new MyPromise( (resolve,reject) => {
-		resolve('other')
-	})
-}
-
-promiseV4.then( (value1) => {
-	console.log( `value1的参数是：`, value1 )
-	// return '这是value1的返回值' // 返回普通纸
-	return other()  // 返回promise对象
-} ).then( (value2) => {
-	console.log(`value2的参数是：`, value2)
+p1.then( ( value ) => {
+	console.log(`value`, value)
+}, (reason) => {
+	console.log(`reason`, reason)
 })
