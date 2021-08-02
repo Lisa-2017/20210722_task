fedb072c374bc204db85fb87ad593263dfdf031d
wb-lx652828 <wb-lx652828@alibaba-inc.com>
2021-08-02 19:16:41 +0800

    feat: 手写Promise —— 捕获错误 & 处理错误 : 执行器/then回调。其中then回调函数中又分为：成功，失败，异步三种情况

--- part1/fed-e-task-01-01/code/testPromise.js
+++ part1/fed-e-task-01-01/code/testPromise.js
@@ -13,6 +13,7 @@
 // } )
 
 
+
 // // MyPromise_V2 测试========================================
 // const MyPromise = require( './MyPromise_V2' );
 // let promise = new MyPromise( (resolve,reject) => {
@@ -26,6 +27,7 @@
 // }, (reason) => {
 //     console.log(`reason`, reason)
 // })
+
 
 
 // // MyPromise_V3 测试========================================
@@ -60,6 +62,7 @@
 // })
 
 
+
 // // MyPromise_V4 测试========================================
 // const MyPromise = require( './MyPromise_V4' );
 // let promiseV4 = new MyPromise( (resolve,reject) => {
@@ -83,20 +86,98 @@
 
 
 
+// // MYPromise_V5 测试========================================
+// const MyPromise = require( './MyPromise_V5' );
+// let promise = new MyPromise( (resolve,reject) => {
+// 	resolve('测试promise返回')
+// })
 
-// MYPromise_V5 测试========================================
-const MyPromise = require( './MyPromise_V5' );
+// let p1 = promise.then( value => {
+// 	console.log(`value`, value)
+// 	return p1
+// } )
+
+// p1.then( ( value ) => {
+// 	console.log(`value`, value)
+// }, (reason) => {
+// 	console.log(`reason`, reason)
+// })
+
+
+
+// // MyPromise_V6_1 测试 执行器中错误捕获====================================
+// const MyPromise = require( './MyPromise_V6' );
+// let promise = new MyPromise( (resolve,reject) => {
+// 	throw new Error( '测试执行器错误' );
+// 	resolve(1)
+// } )
+
+// promise.then( value => {
+// 	console.log( `value1`, value )
+// }, reason => {
+// 	// console.log(`reason`, reason)
+// 	console.log( `reason1`, reason.message ) //捕获到了执行器函数中的错误
+// } )
+
+
+
+// // MyPromise_V6_2 测试 then方法的回调函数中 resolve  时的错误捕获====================================
+// const MyPromise = require( './MyPromise_V6' );
+// let promise = new MyPromise( (resolve,reject) => {
+// 	resolve(1)
+// } )
+// promise.then( value => {
+// 	console.log( `value1`, value )
+// 	throw new Error('测试 then方法的回调中发生错误')
+// }, reason => {
+// 	console.log(`reason1`, reason)
+// } ).then( value => {
+// 	console.log(`value2`, value)
+// }, reason => {
+// 	// console.log(`reason2`, reason)
+// 	console.log(`reason2-这里捕获到的是上一个then的回调函数中发生的错误`, reason.message)
+// } )
+
+
+
+// // MyPromise_V6_3 测试 then方法的回调函数中 rejected 时的错误捕获==================================
+// const MyPromise = require( './MyPromise_V6' );
+// let promise = new MyPromise( (resolve,reject) => {
+// 	reject('失败了')
+// } )
+// promise.then( value => {
+// 	console.log( `value1`, value )
+// }, reason => {
+// 	console.log(`reason1`, reason)
+// 	return '当前promise虽然失败了，但是仍然向下一个promise中传递数据'
+// 	// throw new Error('THEN ERROR') // 测试 then 返回失败
+// } ).then( value => {
+// 	console.log(`value2-- 上一个then 方法返回的只要是 一般值或者promise对象，我都接收：`, value)
+// }, reason => {
+// 	// console.log(`reason2`, reason)
+// 	console.log(`reason2-上一个then方法返回的是失败的就交给我吧：`, reason.message)
+// } )
+
+
+
+// MyPromise_V6_4 测试 异步错误捕获====================================
+const MyPromise = require( './MyPromise_V6' );
 let promise = new MyPromise( (resolve,reject) => {
-	resolve('测试promise返回')
-})
-
-let p1 = promise.then( value => {
-	console.log(`value`, value)
-	return p1
+	setTimeout( () => {
+		resolve('测试异步的错误捕获--成功')
+	},2000)
 } )
 
-p1.then( ( value ) => {
-	console.log(`value`, value)
-}, (reason) => {
-	console.log(`reason`, reason)
-})
+promise.then( value => {
+	console.log( `value1`, value )
+	return 'aaa--测试异步错误捕获'
+}, reason => {
+	console.log(`reason1`, reason)
+} ).then( value => {
+	console.log(`value2`, value)
+}, reason => {
+	console.log(`reason2`, reason)
+} )
+
+
+
